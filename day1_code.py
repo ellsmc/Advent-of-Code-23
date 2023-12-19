593ff7dadf5171c8a42f9886a423689c6e5a5f5c
Ellen C <ellenchapman999@gmail.com>
2023-12-19 17:46:45 +0000

    dropped input size, duplicate single digits

--- day1_code.py
+++ day1_code.py
@@ -1,3 +1,5 @@
+import re
+
 def text_to_list(filename):
     data_lists = []
     with open(filename, "r") as file:
@@ -6,4 +8,28 @@ def text_to_list(filename):
             data_lists.append(data)
     return data_lists
 
-print(text_to_list('day1_input.txt'))
+def extract_numbers(item):
+    numbers = re.findall('[0-9]+', item)
+    numbers = [int(element) for element in numbers]
+
+    return numbers
+
+def calculate_calibration(numbers_list):
+    list_to_add = []
+    for sublist in numbers_list:
+        if len(sublist) == 1 and len(str(sublist[0])) == 1:
+            list_to_add.append(int(str(sublist[0]) * 2))
+        elif len(sublist) > 1:
+            list_to_add.append(int(str(sublist[0]) + str(sublist[-1])))
+    return list_to_add
+
+
+######
+# sum of smaller day 1 should be 408
+#####
+
+calibration_values = []
+for entry in text_to_list('smallerday1.txt'):
+    calibration_values.append(extract_numbers(entry))
+print(calibration_values)
+print(calculate_calibration(calibration_values))
